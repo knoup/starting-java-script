142c68f5c3c38017b29a4590ea8ab524cc2a9ec4
knoup <knoup@pm.me>
2021-02-01 10:53:41 -0300

    initial commit

---
+++ comentarios.js
@@ -0,0 +1,12 @@
+// Aqui eu tenho um comentário em linha
+
+/*
+    Aqui eu tenho um comentário multi linha
+
+    o código abaixo escreve alguma mensagem no devtools
+
+*/
+
+
+console.log('Bem vindos ao Starter') // alguma coisa
+console.log('Não iria ignorar')
---
+++ functions/functions_2.js
@@ -0,0 +1,12 @@
+// fuction expression
+// function anonymous
+
+// parâmetros (parameters)
+const sum = function(number1, number2) {
+    let total = number1 + number2
+    return total
+}
+
+let number1 = 10
+let number2 = 20
+console.log(`A soma de ${number1} com ${number2} é ${sum(number1, number2)}`)
